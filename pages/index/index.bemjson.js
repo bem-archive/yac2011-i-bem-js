0c31b1c911c3cab3e251d75b04c96c49c1d390eb
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-06 23:29:57 +0400

    Block b-cut on the page

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -5,9 +5,23 @@
         { elem: 'css', url: 'index.css'},
         { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8'},
         { block: 'i-jquery', elem: 'core'},
-        { elem: 'js', url:'index.js'},
+        { elem: 'js', url:'index.js'}
     ],
     content: [
-        'block content'
+        {
+            block: 'b-cut',
+            opener: 'открыть кат',
+            content: [
+                {
+                    block: 'b-text',
+                    content: [
+                        {
+                            elem: 'p',
+                            content: 'Мама мыла раму'
+                        }
+                    ]
+                }
+            ]
+        }
     ]
 })
