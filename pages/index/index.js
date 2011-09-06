2f2b22c59e8430a08a813e33297646b3eed9d2dc
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-06 16:50:00 +0400

    First page

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+/** @requires BEM */
+/** @requires BEM.DOM */
+
+(function() {
+
+BEM.DOM.decl('index', {
+
+    onSetMod : {
+        'js' : function() {
+            /* ... */
+        }
+    }
+
+}, {
+
+    live : function() {
+        /* ... */
+    }
+
+});
+
+})();
